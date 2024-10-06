39df473a4d15b3d9c35e6de4c200e0cdf853773b
RobertHumolli <120424157+RobertHumolli@users.noreply.github.com>
2024-10-06 18:08:25 +0100

    3

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,34 +1,54 @@
 using System.Collections;
 using System . Collections . Generic ;
 using UnityEngine ;
-using UnityEngine . UI ;
-using TMPro ;
-public class PlayerController : MonoBehaviour{
-public float speed;
-private int count;
-private int numPickups = 4; // Put here the number of pickups you have .
-public TextMeshProUGUI scoreText;
-public TextMeshProUGUI winText;
-void Start()
+using UnityEngine . InputSystem ;
+using UnityEngine.UI;
+using TMpro;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Vector2 moveValue;
+    public float speed;
+    private int count;
+    private int numPickups = 0;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+
+    void Start()
     {
         count = 0;
-        winText.text = " ";
-        SetCountText();
-      }
-void FixedUpdate() { ...} // NO changes
-void OnTriggerEnter(Collider other){
+        winText.text = "";
+        SetCountText ();
+    }
+    void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.
+        fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
         if (other.gameObject.tag == " PickUp ")
         {
             other.gameObject.SetActive(false);
             count++;
             SetCountText();
-            }
         }
-private void SetCountText(){
-        scoreText.text = " Score : " + count.ToString();
-        if (count >= numPickups)
+    }
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if(count >= numPickups)
         {
-            winText.text = " You win ! ";
-            }
+            winText.text = "You win!";
         }
+    }
+ 
 }
