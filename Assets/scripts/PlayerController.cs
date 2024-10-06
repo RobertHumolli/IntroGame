e03f32339648ff8267a0d98f3619213fce5d2b89
RobertHumolli <120424157+RobertHumolli@users.noreply.github.com>
2024-10-06 17:58:03 +0100

    Text

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,44 +1,34 @@
 using System.Collections;
 using System . Collections . Generic ;
 using UnityEngine ;
-using UnityEngine . InputSystem ;
-
-public class PlayerController : MonoBehaviour
-{
-
-    public Vector2 moveValue;
-    public float speed;
-    private int count;
-
-    private void Start()
+using UnityEngine . UI ;
+using TMPro ;
+public class PlayerController : MonoBehaviour{
+public float speed;
+private int count;
+private int numPickups = 4; // Put here the number of pickups you have .
+public TextMeshProUGUI scoreText;
+public TextMeshProUGUI winText;
+void Start()
     {
         count = 0;
-
-    }
-    void OnMove(InputValue value)
-    {
-        moveValue = value.Get<Vector2>();
-    }
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-
-        GetComponent<Rigidbody>().AddForce(movement * speed * Time.
-        fixedDeltaTime);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
+        winText.text = " ";
+        SetCountText();
+      }
+void FixedUpdate() { ...} // NO changes
+void OnTriggerEnter(Collider other){
         if (other.gameObject.tag == " PickUp ")
         {
-            Destroy(other.gameObject);
-           
-            count += 1;
-
-            Debug.Log("Pickups collected: " + count);
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+            }
         }
-    }
-
- 
+private void SetCountText(){
+        scoreText.text = " Score : " + count.ToString();
+        if (count >= numPickups)
+        {
+            winText.text = " You win ! ";
+            }
+        }
 }
